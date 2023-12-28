7bfbcc79efd8f34513af513e240950e1a3adf217
kogun <akogun@malumbres.com.cy>
2023-12-28 15:08:53 +0400

    move jlink task to pom

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 SCRIPTPATH=$(dirname "$0")
-$SCRIPTPATH/jre17/bin/java -Xmx2g -cp $SCRIPTPATH/geohammer-jar-with-dependencies.jar com.ugcs.gprvisualizer.app.MainGeoHammer
+$SCRIPTPATH/jre21/bin/java -Xmx4g -cp $SCRIPTPATH/geohammer-jar-with-dependencies.jar com.ugcs.gprvisualizer.app.MainGeoHammer
