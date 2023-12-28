6f707dd55303267bc7e4650b0e12dfc18bb0256a
kogun <akogun@malumbres.com.cy>
2023-12-29 00:33:37 +0400

    fix build for linux & windows

--- start.cmd
+++ start.cmd
@@ -1,2 +1,2 @@
 set INSTALL_PATH=%~dp0
-"%INSTALL_PATH%jre17\bin\java" -Xmx2g -cp "%INSTALL_PATH%geohammer-jar-with-dependencies.jar" com.ugcs.gprvisualizer.app.MainGeoHammer
+"%INSTALL_PATH%jre21\bin\java" -Xmx4g -cp "%INSTALL_PATH%geohammer-jar-with-dependencies.jar" com.ugcs.gprvisualizer.app.MainGeoHammer
