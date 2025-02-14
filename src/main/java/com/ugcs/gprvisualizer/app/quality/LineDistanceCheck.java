52f0c1f207281fa8c3c19fbfb807d77796ccce3a
Evgeny Pisarenko <episarenko@ugcs.com>
2025-02-14 13:27:43 +0100

    Improve line distance check (#176)

--- src/main/java/com/ugcs/gprvisualizer/app/quality/LineDistanceCheck.java
+++ src/main/java/com/ugcs/gprvisualizer/app/quality/LineDistanceCheck.java
@@ -1,12 +1,15 @@
 package com.ugcs.gprvisualizer.app.quality;
 
 import com.github.thecoldwine.sigrun.common.ext.LatLon;
+import com.github.thecoldwine.sigrun.common.ext.SphericalMercator;
 import com.ugcs.gprvisualizer.app.parcers.GeoData;
 import com.ugcs.gprvisualizer.utils.Range;
+import javafx.geometry.Point2D;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class LineDistanceCheck implements QualityCheck {
 
@@ -40,12 +43,17 @@ public class LineDistanceCheck implements QualityCheck {
             if (previousLineIndex == null) {
                 continue;
             }
+
+            Range range = e.getValue();
+            Range previousRange = lineSchema.getRanges().get(previousLineIndex);
             LineComponents previousLine = lineSchema.getComponents().get(previousLineIndex);
-            Range lineRange = e.getValue();
+            // index of the previous line points
+            // ordered by projection scalar relative to line origin point
+            TreeMap<Double, LatLon> index = buildLineIndex(values, previousRange, previousLine);
 
             LatLon issueFrom = null;
             LatLon issueTo = null;
-            for (int i = lineRange.getMin().intValue(); i <= lineRange.getMax().intValue(); i++) {
+            for (int i = range.getMin().intValue(); i <= range.getMax().intValue(); i++) {
                 GeoData value = values.get(i);
                 LatLon latlon = new LatLon(value.getLatitude(), value.getLongitude());
                 if (issueTo != null && latlon.getDistance(issueTo) < DISTANCE_THRESHOLD) {
@@ -53,8 +61,10 @@ public class LineDistanceCheck implements QualityCheck {
                     continue;
                 }
 
-                double distanceToPreviousLine = previousLine.getDistanceToPoint(latlon);
-                if (distanceToPreviousLine > max + tolerance) {
+                LatLon nearest = getNearestToProjection(index, previousLine, latlon);
+                boolean hasIssue = nearest != null
+                        && nearest.getDistance(latlon) > max + tolerance;
+                if (hasIssue) {
                     issueTo = latlon;
                     if (issueFrom == null) {
                         issueFrom = issueTo;
@@ -74,6 +84,51 @@ public class LineDistanceCheck implements QualityCheck {
             }
         }
         return issues;
+    }
+
+    private double getProjectionScalar(LineComponents line, Point2D point) {
+        Point2D ab = point.subtract(line.getPoint());
+        return ab.dotProduct(line.getDirection());
+    }
+
+    private TreeMap<Double, LatLon> buildLineIndex(List<GeoData> values, Range range, LineComponents line) {
+        int from = range.getMin().intValue();
+        int to = range.getMax().intValue();
+
+        TreeMap<Double, LatLon> index = new TreeMap<>();
+        for (int i = from; i <= to; i++) {
+            GeoData value = values.get(i);
+            LatLon point = new LatLon(value.getLatitude(), value.getLongitude());
+            double d = getProjectionScalar(line, SphericalMercator.project(point));
+            index.put(d, point);
+        }
+        return index;
+    }
+
+    private LatLon getNearestToProjection(TreeMap<Double, LatLon> index, LineComponents line, LatLon point) {
+        double d = getProjectionScalar(line, SphericalMercator.project(point));
+
+        LatLon exact = index.get(d);
+        if (exact != null) {
+            return exact;
+        }
+
+        // on the left and right from the point projection
+        // are two candidates, select one with the min
+        // distance to d
+        Map.Entry<Double, LatLon> l = index.lowerEntry(d);
+        Map.Entry<Double, LatLon> r = index.higherEntry(d);
+        if (l != null) {
+            if (r != null) {
+                return Math.abs(d - l.getKey()) <= Math.abs(d - r.getKey())
+                        ? l.getValue()
+                        : r.getValue();
+            } else {
+                return l.getValue();
+            }
+        } else {
+            return r != null ? r.getValue() : null;
+        }
     }
 
     private QualityIssue createStripeIssue(LatLon from, LatLon to, LineComponents referenceLine) {
